cb3f6fb66489b528240c281c623d15c39f1afb20
Javier <javierLhernandez@yahoo.com>
2024-12-31 17:07:34 -0500

    redoing layerTree as array

--- play-file2.js
+++ play-file2.js
@@ -7,6 +7,18 @@ const root = {
     6: []
 }
 
-function bfsLayerTree(root, start) {
-    
+function bfsLayerTree(root) {
+    const queueList = []
+    queueList.push(root)
+    console.log("queueList", queueList)
+    while(!queueList.length === 0) {
+        let currentLevel = []
+        const levelSize = queueList.length;
+
+        // for(let i = 0; i < levelSize; i++) {
+        //     const node = qu
+        // }
+    }
 }
+
+bfsLayerTree(root)
