addf062ab326a89db7f0acd62f79249af6b834c8
Javier <javierLhernandez@yahoo.com>
2024-12-31 19:52:40 -0500

    built out Queue class

--- play-file3.js
+++ play-file3.js
@@ -7,3 +7,23 @@ const root = {
     5: [],
     6: []
 }
+
+class Queue {
+    constructor() {
+        let items = []
+    }
+    enqueue(el) {
+        this.items.push(el)
+    }
+    dequeue() {
+        //if isEmpty is true, return error
+        if(this.isEmpty()) {
+            console.log("QUEUE IS EMPTY")
+            throw new Error("QUEUE IS EMPTY")
+        }
+        return this.items.shift();
+    }
+    isEmpty() { //returns true if there are 0 el's in items
+        return this.items.length === 0;
+    }
+}
