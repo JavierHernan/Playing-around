1bbf1b68624b8788ef8a0ceca71c30ec65aebd3b
Javier <javierLhernandez@yahoo.com>
2025-01-03 23:54:24 -0500

    hashmaps, for in loop practice

--- play-file6.js
+++ play-file6.js
@@ -1,5 +1,15 @@
 //hash map
-//for in
-//for of
+//for in ... THESE ARE USED FOR OBJECTS
+//for of ... THESE ARE USED FOR ARRAYS
 //if in
-let hashMap = {}
+// let hashMap = {}
+let hashMap = {
+    key1: "val1",
+    key2: "val2",
+    key3: "val3"
+}
+
+for(let key in hashMap) {
+    console.log("key", key)
+    console.log("hash[key]", hashMap[key])
+}
