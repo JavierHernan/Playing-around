c1f270679a4143e244088d4390bd19e509f9ab3a
Javier <javierLhernandez@yahoo.com>
2025-01-03 23:56:25 -0500

    trying something else

--- play-file6.js
+++ play-file6.js
@@ -2,14 +2,16 @@
 //for in ... THESE ARE USED FOR OBJECTS
 //for of ... THESE ARE USED FOR ARRAYS
 //if in
-// let hashMap = {}
-let hashMap = {
-    key1: "val1",
-    key2: "val2",
-    key3: "val3"
-}
+let hashMap = {}
+// let hashMap = {
+//     key1: "val1",
+//     key2: "val2",
+//     key3: "val3"
+// }
 
-for(let key in hashMap) {
-    console.log("key", key)
-    console.log("hash[key]", hashMap[key])
-}
+// for(let key in hashMap) {
+//     console.log("key", key)
+//     console.log("hash[key]", hashMap[key])
+// }
+
+
