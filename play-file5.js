f9bc6d83b6b4119c6afbaa528be64805a721d697
Javier <javierLhernandez@yahoo.com>
2025-01-03 22:36:55 -0500

    playing with classes, making a character thing

---
+++ play-file5.js
@@ -0,0 +1,46 @@
+//mess around with classes and constructors
+class Armor {
+    constructor(slashN, pierceN) {
+        this.slashNegate = slashN;
+        this.pierceNegate = pierceN;
+    }
+}
+class Weapon {
+    constructor(slash, pierce) {
+        this.slashDamage = slash;
+        this.pierceDamage = pierce;
+    }
+}
+class FancyHat {
+    constructor(swagFactor) {
+        this.swagFactor = swagFactor;
+    }
+}
+
+class Character {
+    constructor() {
+        this.armor;
+        this.weapon;
+        this.fancyHat; 
+    }
+    equipArmor(item) {
+        this.armor = item
+    }
+    equipWeapon(item) {
+        this.weapon = item
+    }
+    wearFancyHat(item) {
+        this.fancyHat = item
+    }
+}
+
+let ChainMail = new Armor(5,0);
+console.log(ChainMail)
+let ME = new Character();
+ME.equipArmor(ChainMail)
+console.log("ME.equipArmor", ME)
+let Spear = new Weapon(0,6);
+ME.equipWeapon(Spear)
+console.log("ME.equipWeapon", ME)
+// let Fedora = new FancyHat();
+
