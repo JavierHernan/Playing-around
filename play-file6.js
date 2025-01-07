dea662094506ec9c6f53d909c8ad901d38882323
Javier <javierLhernandez@yahoo.com>
2025-01-06 19:08:12 -0500

    building out hashmap solution

--- play-file6.js
+++ play-file6.js
@@ -2,6 +2,10 @@
 //for in ... THESE ARE USED FOR OBJECTS
 //for of ... THESE ARE USED FOR ARRAYS
 //if in
+
+//iterate through an array, keep track of repeating elements in array using a hashmap
+//then return object el with highest count
+let arr = [1,1,2,3,4,4,4,5,6,7,7,7,7,8,9,9];
 let hashMap = {}
 // let hashMap = {
 //     key1: "val1",
@@ -14,4 +18,19 @@ let hashMap = {}
 //     console.log("hash[key]", hashMap[key])
 // }
 
+const repeatCountFunc = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        console.log("arr[i]", arr[i])
+        if(!hashMap[arr[i]]) {
+            hashMap[arr[i]] = 1
+        } else {
+            hashMap[arr[i]] += 1
+        }
+    }
+    // console.log("hashMap", hashMap)
+    for(let count in hashMap) {
+        console.log("count", count)
+    }
+}
 
+repeatCountFunc(arr)
