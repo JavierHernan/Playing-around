94cc58ad6c834e3be0f037ce9950cb2eb9657d1a
Javier <javierLhernandez@yahoo.com>
2025-01-10 04:58:18 -0500

    redoing print each layer of a tree using bfs

--- play-file1.01.js
+++ play-file1.01.js
@@ -16,3 +16,30 @@ let tree = {
 //a class must take this.items in constructor. methods go after constructor and are modifying this.items
 
 //
+class Queue {
+    constructor() {
+        this.items = []
+    }
+    enqueue(el) { //adds item to items
+        this.items.push(el)
+    }
+    dequeue() { //removes last item 
+
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
+
+function printEveryLayer(tree) {
+    //new instance of Queue
+    const queue = new Queue()
+    
+    //add tree to queue via enqueu
+    queue.enqueue(tree)
+
+    //loop over queue while queue is not Empty
+    while(!queue.isEmpty()) {
+        //
+    }
+}
