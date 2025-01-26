a4164f8f14f3d442a522a1e6416823b49fb02ae3
Javier <javierLhernandez@yahoo.com>
2025-01-26 04:24:15 -0500

    refreshing python

--- play-file1.py
+++ play-file1.py
@@ -1,3 +1,25 @@
 print("Hello")
 
+#declare a variable
+age = 29
+name = "Javier"
+height = 5.8
+is_tough = True
+x, y, z = 1, 2, 3
+# write a function
+
+# write a conditional
+if age > 25:
+    print(f"{name}, is older than 25")
+elif age == 25:
+    print(f"{name}, is 25")
+else:
+    print(f"{name}, is less than 25")
+
+if name == "Javier" and age == 29:
+    print("Javier is 29")
+
+if height == 5.9 or is_tough == True:
+    print("You are either 5.9 or you are tough")
+
 # python3 play-file1.py
