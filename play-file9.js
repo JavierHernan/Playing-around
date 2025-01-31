f79f24c6ae8cad899d3c0b6ac3d31f4ff6458411
Javier <javierLhernandez@yahoo.com>
2025-01-30 23:43:24 -0500

    fizzbuzz

--- play-file9.js
+++ play-file9.js
@@ -5,10 +5,20 @@ let fizzbuzz = "FizzBuzz";
 let fiz = "fiz";
 
 let FizzBuzzFunc = (arg) => {
-    let argFormatted = arg.toUpperCase()
-    console.log("arg", argFormatted)
+    let argFormatted = arg.toUpperCase();
+    // console.log("arg", argFormatted)
+    // if(argFormatted == "FIZZ") {
+    //     console.log("Fizz")
+    // } else if (argFormatted == "BUZZ") {
+    //     console.log("Buzz")
+    // } else if (argFormatted == "FIZZBUZZ") {
+    //     console.log("FizzBuzz")
+    // }
+    argFormatted == "FIZZ" ? console.log("Fizz") 
+  : argFormatted == "BUZZ" ? console.log("Buzz")
+  : argFormatted == "FIZZBUZZ" ? console.log("FizzBuzz")
+  : null
 }
-
 let try1 = FizzBuzzFunc(fizz)
 let try2 = FizzBuzzFunc(buzz)
 let try3 = FizzBuzzFunc(fizzbuzz)
