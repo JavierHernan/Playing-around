bc5b88ec05b31a2a8e2adc7b50896266448a9942
Javier <javierLhernandez@yahoo.com>
2025-03-15 22:51:02 -0400

    redid FizzBuzz

--- play-file9.js
+++ play-file9.js
@@ -1,26 +1,46 @@
 //FizzBuzz
-let fizz = "Fizz";
-let buzz = "Buzz";
-let fizzbuzz = "FizzBuzz";
-let fiz = "fiz";
+// let fizz = "Fizz";
+// let buzz = "Buzz";
+// let fizzbuzz = "FizzBuzz";
+// let fiz = "fiz";
+
+// let FizzBuzzFunc = (arg) => {
+//     let argFormatted = arg.toUpperCase();
+//     // if(argFormatted == "FIZZ") {
+//     //     console.log("Fizz")
+//     // } else if (argFormatted == "BUZZ") {
+//     //     console.log("Buzz")
+//     // } else if (argFormatted == "FIZZBUZZ") {
+//     //     console.log("FizzBuzz")
+//     // }
+//     argFormatted == "FIZZ" ? console.log("Fizz") 
+//   : argFormatted == "BUZZ" ? console.log("Buzz")
+//   : argFormatted == "FIZZBUZZ" ? console.log("FizzBuzz")
+//   : null
+// }
+// let try1 = FizzBuzzFunc(fizz)
+// let try2 = FizzBuzzFunc(buzz)
+// let try3 = FizzBuzzFunc(fizzbuzz)
+// let try4 = FizzBuzzFunc(fiz)
+
+
+// True FizzBuzz
+// If divisible by 3, Fizz
+// If divisble by 5, Buzz
+// If divisble by 5 and 3, FizzBuzz
 
 let FizzBuzzFunc = (arg) => {
-    let argFormatted = arg.toUpperCase();
-    // if(argFormatted == "FIZZ") {
-    //     console.log("Fizz")
-    // } else if (argFormatted == "BUZZ") {
-    //     console.log("Buzz")
-    // } else if (argFormatted == "FIZZBUZZ") {
-    //     console.log("FizzBuzz")
-    // }
-    argFormatted == "FIZZ" ? console.log("Fizz") 
-  : argFormatted == "BUZZ" ? console.log("Buzz")
-  : argFormatted == "FIZZBUZZ" ? console.log("FizzBuzz")
-  : null
+  if(arg % 3 === 0 && arg % 5 === 0) {
+    return "FizzBuzz"
+  } else if(arg % 3 === 0) {
+    return "Fizz"
+  } else if (arg % 5 === 0) {
+    return "Buzz"
+  }
 }
-let try1 = FizzBuzzFunc(fizz)
-let try2 = FizzBuzzFunc(buzz)
-let try3 = FizzBuzzFunc(fizzbuzz)
-let try4 = FizzBuzzFunc(fiz)
+console.log("invocation 1 (15):", FizzBuzzFunc(15))
+console.log("invocation 2 (3):",FizzBuzzFunc(3))
+console.log("invocation 3 (5):",FizzBuzzFunc(5))
+
 
 // node play-file9.js
