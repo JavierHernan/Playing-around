1a2c819965080c37750c8d1f595b01fc18e0c0df
Javier <javierLhernandez@yahoo.com>
2025-04-12 20:18:44 -0400

    bfs in python

---
+++ play-file2.py
@@ -0,0 +1,22 @@
+# //          1
+# //      2       3
+# //    4       5    6
+# //              7
+tree = {
+    1: [2,3],
+    2: [4, None],
+    3: [5, 6],
+    4: [],
+    5: [None, 7],
+    6: [],
+    7: []
+}
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, el):
+        self.items.push(el)
+
+# python3 play-file2.py
