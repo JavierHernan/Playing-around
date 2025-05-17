835a25016cdeafe6d4cf5d2f5fdec42ebad9ba23
Javier <javierLhernandez@yahoo.com>
2025-05-16 22:51:55 -0400

    character class modifications

--- play-file5BattleGame/play-file5Character.js
+++ play-file5BattleGame/play-file5Character.js
@@ -1,29 +1,29 @@
 //mess around with classes and constructors
 
-class Character {
-    constructor() {
-        this.armor;
-        this.weapon;
-        this.fancyHat; 
-    }
-    equipArmor(item) {
-        this.armor = item
-    }
-    equipWeapon(item) {
-        this.weapon = item
-    }
-    wearFancyHat(item) {
-        this.fancyHat = item
-    }
-    attack(){
-        if(this.weapon.pierceDamage && this.weapon.slashDamage){
-            console.log(`You have done ${this.weapon.pierceDamage} in pierce and ${this.weapon.slashDamage} in slash`)
-        } else if(this.weapon.pierceDamage && !this.weapon.slashDamage) {
-            console.log(`You have done ${this.weapon.pierceDamage} in pierce`)
-        } else if (!this.weapon.pierceDamage && this.weapon.slashDamage) {
-            console.log(`You have done ${this.weapon.pierceDamage} in slash`)
-        }
-    }
-}
+// class Character {
+//     constructor() {
+//         this.armor;
+//         this.weapon;
+//         this.fancyHat; 
+//     }
+//     equipArmor(item) {
+//         this.armor = item
+//     }
+//     equipWeapon(item) {
+//         this.weapon = item
+//     }
+//     wearFancyHat(item) {
+//         this.fancyHat = item
+//     }
+//     attack(){
+//         if(this.weapon.pierceDamage && this.weapon.slashDamage){
+//             console.log(`You have done ${this.weapon.pierceDamage} in pierce and ${this.weapon.slashDamage} in slash`)
+//         } else if(this.weapon.pierceDamage && !this.weapon.slashDamage) {
+//             console.log(`You have done ${this.weapon.pierceDamage} in pierce`)
+//         } else if (!this.weapon.pierceDamage && this.weapon.slashDamage) {
+//             console.log(`You have done ${this.weapon.pierceDamage} in slash`)
+//         }
+//     }
+// }
 
 // node play-file5BattleGame.js
