bb3d7b62f4c3430180b5c63e17197f640f2b3b7c
Javier <javierLhernandez@yahoo.com>
2025-05-18 21:17:07 -0400

    adding imports

--- play-file5BattleGame/play-file5.03CreateItems.js
+++ play-file5BattleGame/play-file5.03CreateItems.js
@@ -1,1 +1,4 @@
 import Character from './Character.js';
+import Weapon from './play-file5.01Weapons.js';
+import Armor from './play-file5.01Armor.js';
+import FancyHat from './play-file5.02Hats.js';
