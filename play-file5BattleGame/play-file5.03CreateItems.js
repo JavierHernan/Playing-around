aae82890a6698388fcde270d954ca7b25c53c4f4
Javier <javierLhernandez@yahoo.com>
2025-05-18 21:17:55 -0400

    creating first items

--- play-file5BattleGame/play-file5.03CreateItems.js
+++ play-file5BattleGame/play-file5.03CreateItems.js
@@ -2,3 +2,7 @@ import Character from './Character.js';
 import Weapon from './play-file5.01Weapons.js';
 import Armor from './play-file5.01Armor.js';
 import FancyHat from './play-file5.02Hats.js';
+
+let Spear = new Weapon(0,6);
+Spear.shaftLength = 5;
+let ChainMail = new Armor(5,0);
