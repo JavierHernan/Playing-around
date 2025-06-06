0f9ce46468d67410ed2058e1e257e74acd022cf4
Javier <javierLhernandez@yahoo.com>
2025-06-05 22:50:03 -0400

    added stats to spear

--- play-file5BattleGame/play-file5.03CreateItems.js
+++ play-file5BattleGame/play-file5.03CreateItems.js
@@ -3,7 +3,7 @@ import Weapon from './play-file5.01Weapons.js';
 import Armor from './play-file5.01Armor.js';
 import FancyHat from './play-file5.02Hats.js';
 
-let Spear = new Weapon(0,6);
+let Spear = new Weapon(0,6, 3, "throw");
 Spear.shaftLength = 5;
 let ChainMail = new Armor(5,0);
 let Sword = new Weapon(6, 2, 3, "parry")
